f22a9eccde118979209bd7f43dc816f1a2c87408
Rob Prouse <rob@prouse.org>
2018-10-22 20:15:32 -0400

    Add project files.

---
+++ hue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
